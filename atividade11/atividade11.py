97990b1343ab4924010fca43b77d263e96a44c1c
kayky01silva <kaykysilva232334@gmail.com>
2024-09-11 14:52:38 -0300

    atividade11

---
+++ atividade11/atividade11.py
@@ -0,0 +1,12 @@
+# Crie um programa que receba a idade de uma pessoa e classifique-a de acordo com as seguintes faixas etárias:
+# Criança (0-12 anos)
+# Adolescente (13-17 anos)
+# Adulto (18-59 anos)
+# Idoso (60 anos ou mais)
+
+
+idade = int(input('coloque sua idade'))
+if idade <=12:print ("voç~e é criança")
+elif idade <=13-17:print("veçê é adolecente")
+elif idade >=18-59:print("voçê é adulto")
+elif idade >=60:print('voçê e idoso')
